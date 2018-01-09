192144662066375ddf68eee91f836846152cee5e
RajaSoufi <rjsoufi@gmail.com>
2018-01-09 11:28:01 +0100

    Fixed typo in file_download.go

--- gossiper/file_download.go
+++ gossiper/file_download.go
@@ -447,7 +447,7 @@ func startDownload(g *Gossiper, filereq *common.FileRequest) {
 		// the file has been sent by Uploader, and its signature by origin is invalid
 		// action : decrease reputation of Uploader
 
-    if record, ok := g.keyRing.GetRecord(*fileReq.Origin) ; ok {
+    if record, ok := g.keyRing.GetRecord(*filereq.Origin) ; ok {
       g.reputationTable.DecreaseSigRep(filereq.Destination, record.Confidence)
     }
 
@@ -455,7 +455,7 @@ func startDownload(g *Gossiper, filereq *common.FileRequest) {
 		// the file has been sent by Uploader, and its signature by origin is valid
 		// action : increase reputation of Uploader
 
-    if record, ok := g.keyRing.GetRecord(*fileReq.Origin) ; ok {
+    if record, ok := g.keyRing.GetRecord(*filereq.Origin) ; ok {
       g.reputationTable.IncreaseSigRep(filereq.Destination, record.Confidence)
     }
 
